beea00b546211d40af1f0acd5d0530330f68d480
Kossin1337 <piotrswobodzinski@gmail.com>
2022-03-07 10:08:56 +0100

    Added search crypto functionality

--- src/components/cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/cryptocurrencies/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Coin } from "./Coin";
 import { CoinRowHeaders } from "./CoinRowHeaders";
 import "./Cryptocurrencies.scss";
@@ -7,14 +7,32 @@ import { useGetCryptosQuery } from "../../services/cryptoAPI";
 
 export const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
+  const [searchTerm, setSearchTerm] = useState("");
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
   // console.log(cryptos);
+
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
 
   if (isFetching) return <span>Loading...</span>;
 
   return (
     <div className="cryptocurrencies">
+      {!simplified && (
+        <div className="search-crypto">
+          <input
+            placeholder="Search Cryptocurrencies"
+            type="text"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
       <CoinRowHeaders />
       {cryptos?.map((currency) => (
         <Coin currency={currency} key={currency.id}></Coin>
